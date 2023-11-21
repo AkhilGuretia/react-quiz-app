ea4ac2303d30f40308d469fb8258579ff43c9db6
Akhil Kumar <akhilguretia@gmail.com>
2023-11-22 03:48:48 +0530

    Displaying the questions

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,5 +1,18 @@
-const Questions = () => {
-  return <div>Questions</div>;
+/* eslint-disable react/prop-types */
+const Questions = ({ question }) => {
+  return (
+    <div>
+      <h4> {question.question}</h4>
+
+      <div className="options">
+        {question.options.map((option) => (
+          <button className="btn btn-option" key={option}>
+            {option}
+          </button>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Questions;
