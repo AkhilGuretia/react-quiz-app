80118474cb7a6e53e7a45d1b3c6ef5766453c7c3
Akhil Kumar <akhilguretia@gmail.com>
2023-11-23 06:20:22 +0530

    Update title of the app

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -3,7 +3,9 @@ import { useEffect } from "react";
 // eslint-disable-next-line react/prop-types
 const Timer = ({ timeTillAutoSubmit, dispatch }) => {
   const mins = Math.floor(timeTillAutoSubmit / 60);
+
   const secs = timeTillAutoSubmit % 60;
+
   useEffect(() => {
     const id = setInterval(() => {
       dispatch({ type: "TickTock" });
